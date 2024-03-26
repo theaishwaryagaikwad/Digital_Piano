305c47a609f9dcc242e605aa22f8e0a3aead48cb
theaishwaryagaikwad <160391960+theaishwaryagaikwad@users.noreply.github.com>
2024-03-26 11:45:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,37 +25,37 @@ function playSound(button) {
 
     switch (button) {
         case 'a':
-            let sound1 = new Audio('sounds/audio1.mp3');
+            let sound1 = new Audio('audio1.mp3');
             sound1.play();
             break;
 
         case 's':
-            let sound2 = new Audio('sounds/audio2.mp3');
+            let sound2 = new Audio('audio2.mp3');
             sound2.play();
             break;
 
         case 'd':
-            let sound3 = new Audio('sounds/audio3.mp3');
+            let sound3 = new Audio('audio3.mp3');
             sound3.play();
             break;
 
         case 'f':
-            let sound4 = new Audio('sounds/audio4.mp3');
+            let sound4 = new Audio('audio4.mp3');
             sound4.play();
             break;
 
         case 'g':
-            let sound5 = new Audio('sounds/audio5.mp3');
+            let sound5 = new Audio('audio5.mp3');
             sound5.play();
             break;
 
         case 'h':
-            let sound6 = new Audio('sounds/audio6.mp3');
+            let sound6 = new Audio('audio6.mp3');
             sound6.play();
             break;
 
         case 'j':
-            let sound7 = new Audio('sounds/audio7.mp3');
+            let sound7 = new Audio('audio7.mp3');
             sound7.play();
             break;
 
